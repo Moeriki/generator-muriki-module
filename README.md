35edd34d9aaa4a4ddc929dd43dbd9dfc6b0e803e
Dieter Luypaert <dieterluypaert@gmail.com>
2016-09-23 10:45:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-muriki-module [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> A scaffold for muriki&#39;s modules.
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-muriki-module using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-muriki-module
+```
+
+Then generate your new project:
+
+```bash
+yo muriki-module
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [Dieter Luypaert]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-muriki-module.svg
+[npm-url]: https://npmjs.org/package/generator-muriki-module
+[travis-image]: https://travis-ci.org/moeriki/generator-muriki-module.svg?branch=master
+[travis-url]: https://travis-ci.org/moeriki/generator-muriki-module
+[daviddm-image]: https://david-dm.org/moeriki/generator-muriki-module.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/moeriki/generator-muriki-module
+[coveralls-image]: https://coveralls.io/repos/moeriki/generator-muriki-module/badge.svg
+[coveralls-url]: https://coveralls.io/r/moeriki/generator-muriki-module
---
+++ generators/app/index.js
@@ -0,0 +1,109 @@
+'use strict';
+
+const path = require('path');
+
+const chalk = require('chalk');
+const promptName = require('inquirer-npm-name');
+const yeoman = require('yeoman-generator');
+const yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+
+  _copyFile(filename) {
+    this.fs.copyTpl(
+      this.templatePath(filename),
+      this.destinationPath(filename),
+      this.answers
+    );
+  },
+
+  templates: [
+    '__test__/.eslintrc.js',
+    'lib/index.js',
+    '.editorconfig',
+    '.eslintrc.js',
+    '.gitignore',
+    '.npmignore',
+    '.npmrc',
+    '.travis.yml',
+    'index.js',
+    'LICENSE',
+    'README.md',
+  ],
+
+  // initializing - Your initialization methods (checking current project state, getting configs, etc)
+  initializing() {
+    this.log(yosay(
+      `Welcome to the luminous ${chalk.red('muriki-module')} generator!`
+    ));
+  },
+
+  // prompting - Where you prompt users for options (where you'd call this.prompt())
+  prompting() {
+    this.answers = {};
+
+    return promptName({
+      name: 'name',
+      message: 'Module name',
+    }, this).then((answers) => {
+      Object.assign(this.answers, answers);
+      this.destinationRoot(path.join(this.env.cwd, answers.name));
+
+      return this.prompt([
+        {
+          name: 'description',
+          message: 'Description',
+        },
+        {
+          name: 'keywords',
+          message: 'Keywords',
+          filter(keywords) {
+            return keywords.split(',')
+              .map((keyword) => keyword.trim())
+              .filter((keyword) => keyword !== '');
+          },
+        },
+        {
+          name: 'repository',
+          message: 'Repository',
+        },
+      ]).then((moreAnswers) => {
+        Object.assign(this.answers, moreAnswers);
+      });
+    });
+  },
+
+  // configuring - Saving configurations and configure the project (creating .editorconfig files and other metadata files)
+
+  // default - If the method name doesn't match a priority, it will be pushed to this group.
+
+  // writing - Where you write the generator specific files (routes, controllers, etc)
+  writing() {
+    this.templates.forEach((filename) => {
+      this._copyFile(filename);
+    });
+
+    this.fs.writeJSON(
+      this.destinationPath('package.json'),
+      Object.assign(
+        this.fs.readJSON(this.templatePath('package.json')),
+        {
+          name: this.answers.name,
+          description: this.answers.description,
+          keywords: this.answers.keywords,
+          repository: this.answers.repository,
+        }
+      )
+    );
+  },
+
+  // conflicts - Where conflicts are handled (used internally)
+
+  // install - Where installation are run (npm, bower)
+  install() {
+    this.npmInstall();
+  },
+
+  // end - Called last, cleanup, say good bye, etc
+
+});
